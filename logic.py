c326853c12ae94066d2fd4fa9bbb5d65b7c62427
Swathi Yegireddi <swathi.yegireddi@gramener.com>
2018-06-27 23:10:50 +0530

    Added persistent sqllite db

--- logic.py
+++ logic.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import requests
 import unicodecsv as csv
+import sqlite3 as db
 from lxml import html
 from sqlalchemy import create_engine
 
@@ -13,8 +14,8 @@ url = 'https://www.moneycontrol.com/stocks/marketinfo/marketcap/bse/index.html'
 # out = csv.writer(open('stocks.csv','wb',))
 # out.writerow(('company', 'link', 'marketcap'))
 
-# Creates an in-memory SQLite database
-engine = create_engine('sqlite://', echo=False)
+# Connect to SQLite DB 
+engine = create_engine('sqlite:///C:\\Users\\GLP-118\\stocks.db')
 
 page = requests.get(url)
 tree = html.fromstring(page.content)
@@ -26,8 +27,8 @@ marketcap = tree.xpath('//table[@class="tbldata14 bdrtpg"]/tr/td[6]/text()')
 df = pd.DataFrame({'company':company, 'link':link, 'marketcap':marketcap})
 
 # Stores data in SQLite DB 
-df.to_sql('companies', con=engine, index=False, if_exists='replace')
-df1 = engine.execute("SELECT * FROM companies").fetchall()
+df.to_sql('companies', con=engine, index=False, if_exists='append')
+df1 = engine.execute("SELECT count(*) FROM companies").fetchall()
 print df1
 
 
