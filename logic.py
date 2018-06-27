aff402d7c9d035433c4e33602bace3be358479d3
Swathi Yegireddi <swathi.yegireddi@gramener.com>
2018-06-28 00:10:36 +0530

    Added SQL insert constraints to avoid duplication

--- logic.py
+++ logic.py
@@ -2,17 +2,10 @@
 
 import pandas as pd
 import requests
-import unicodecsv as csv
-import sqlite3 as db
 from lxml import html
 from sqlalchemy import create_engine
 
-
 url = 'https://www.moneycontrol.com/stocks/marketinfo/marketcap/bse/index.html'
-
-# Opens a csv file in write mode
-# out = csv.writer(open('stocks.csv','wb',))
-# out.writerow(('company', 'link', 'marketcap'))
 
 # Connect to SQLite DB 
 engine = create_engine('sqlite:///C:\\Users\\GLP-118\\stocks.db')
@@ -26,15 +19,27 @@ marketcap = tree.xpath('//table[@class="tbldata14 bdrtpg"]/tr/td[6]/text()')
 
 df = pd.DataFrame({'company':company, 'link':link, 'marketcap':marketcap})
 
-# Stores data in SQLite DB 
+# Unique list of companies from dataframe
+df_uniq_companies = df.company.unique()
+
+# Extract Unique list of companies from SQLite DB
+table_uniq_companies = engine.execute("SELECT distinct company FROM companies").fetchall()
+# Converts 1-element tuples into a list
+table_uniq_companies = [r[0] for r in table_uniq_companies] 
+
+# List of companies which need to be deleted from database to avoid duplicates
+intersection_list = list(set(df_uniq_companies) & set(table_uniq_companies))
+intersection_list = [str(r) for r in intersection_list]
+
+# Deletion script
+# Ensures in subsequent execution of script no duplicate records should be inserted and new data overrides old data.
+delete_sql = 'delete FROM companies where company in ' + str(tuple(intersection_list))
+engine.execute(delete_sql)
+
+# Stores latest scraped data in SQLite DB 
 df.to_sql('companies', con=engine, index=False, if_exists='append')
-df1 = engine.execute("SELECT count(*) FROM companies").fetchall()
-print df1
 
 
-# Writes df to csv file
-# for i, meta in df.iterrows():	
-# 	out.writerow((meta['company'], meta['link'], meta['marketcap']))
 
 
 
